c16d2da3245a86f393935673cd433cfb1da7e808
Nafis-Fahim <nafisf96@gmail.com>
2022-08-19 02:58:32 +0600

    update case value

--- Script/case.js
+++ Script/case.js
@@ -1,8 +1,22 @@
-document.getElementById('btn-casePlus').addEventListener('click',function(){
+function updateCase(isIncrease){
     const caseField = document.getElementById('case-field');
     const caseValueString = caseField.value;
     const previousCaseValue = parseInt(caseValueString);
 
-    const newCaseValue = previousCaseValue + 1;
+    let newCaseValue;
+    if(isIncrease === true){
+        newCaseValue = previousCaseValue + 1;
+    }
+    else{
+        newCaseValue = previousCaseValue - 1;
+    }
     caseField.value = newCaseValue;
+}
+
+document.getElementById('btn-casePlus').addEventListener('click',function(){
+    updateCase(true);
+})
+
+document.getElementById('btn-caseMinus').addEventListener('click',function(){
+    updateCase(false);
 })
